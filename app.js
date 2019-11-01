5334f32a2d0b83d94ca02d7acc8944bd9df3bc73
Sabrina Rocha <delrock876@gmail.com>
2019-10-31 20:20:48 -0700

    main-game

--- app.js
+++ app.js
@@ -1,5 +1,135 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // get inquirer npm
 const inquirer = require(`inquirer`)
 
 // get mysql2 npm
 const mysql = require(`mysql2`)
+
+let userCandy = 0
+let timer = 0
+
+const countDown =_=>{
+ 
+    if(timer < 60){
+
+    setTimeout(() => {
+     timer++
+      console.log(`${timer}`)
+    }, 1000);
+
+    }
+  }
+//functions to run
+const knock =_=>{
+  let mark = Math.floor(Math.random()* 101)
+  let candy = Math.floor(Math.random()* 11)
+  
+  console.log(mark)
+  console.log(candy)
+
+  if(mark <= 50){
+    userCandy-= candy
+  }else if (mark > 50){
+    userCandy+= candy
+  }else if(userCandy <= 0 || timer === 0){
+    console.log('You lost')
+  }
+}
+
+
+inquirer
+  .prompt([
+
+    {type: 'list',
+      message: 'Choose your fate...',
+      name: 'choice',
+      choices: ['KNOCK-KNOCK TRICK-OR-TREAT!', 'RUN AWAY!']
+    },
+
+    {type:'text',
+      message: 'You have ONE MINUTE to trick or treat!',
+      name:'timer'
+    }
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+  let userChoice = answers.choice
+    switch(userChoice){
+      case `KNOCK-KNOCK TRICK-OR-TREAT!`:
+        knock()
+        countDown()
+        break
+
+      case `RUN AWAY!`:
+        run-blsh 
+        break
+      }
+    console.log(answers.choices)
+  });

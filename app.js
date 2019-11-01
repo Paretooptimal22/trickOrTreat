49b3bc03060a8d6da5b46275ceede237e0aaf8ad
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-10-31 19:46:48 -0700

    connecting to the mysql database.

--- app.js
+++ app.js
@@ -3,3 +3,20 @@ const inquirer = require(`inquirer`)
 
 // get mysql2 npm
 const mysql = require(`mysql2`)
+
+//Connecting to the MySql
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: `root`,
+  password: `password`,
+  database: `trickOrTreat_db`
+})
+
+inquirer
+  .prompt([
+    {
+      type: `input`,
+      name: `user`,
+      message: `What is your name?`
+    }
+  ])
